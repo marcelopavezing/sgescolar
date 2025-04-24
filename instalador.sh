893a14ce1f0c457ffa3bcc0f13fecc31d3c79013
Marcelo Ceballos Pavez <marcelo.ceballos@tide.cl>
2025-04-24 18:50:04 -0400

    Inicial

---
+++ instalador.sh
@@ -0,0 +1,6 @@
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
+    composer install --ignore-platform-reqs && ./vendor/bin/sail up -d && npm install --save-dev vite laravel-vite-plugin && npm run build
